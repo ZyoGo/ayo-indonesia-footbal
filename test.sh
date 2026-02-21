2ace175aec5adcb4d11a9db5a880c466d37ee1bc
ZyoGo <academyynetworking@gmail.com>
2026-02-21 17:51:10 +0800

    fix: remove unused file

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/usr/local/go/bin:/home/noodle/go/bin
-
-cd /mnt/d/project/ayo-indonesia
-go run go.uber.org/mock/mockgen -source=internal/club/domain/repository.go -destination=internal/club/mock/repository_mock.go -package=mock
-go test ./internal/club/app/... -v
